c0ed0e7a48cde128e49b73ea60af73f3e9742ac5
cappNcode <cappncode@gmail.com>
2025-03-23 13:18:14 +0100

    added serverless fucntions

--- dist/js/dataFunctions.js
+++ dist/js/dataFunctions.js
@@ -13,14 +13,31 @@ export const getHomeLocation = () => {
 };
 
 export const getWeatherFromCoords = async (locationObj) => {
-  const lat = locationObj.getLat();
-  const lon = locationObj.getLong();
-  const units = locationObj.getUnit();
+  // const lat = locationObj.getLat();
+  // const lon = locationObj.getLong();
+  // const units = locationObj.getUnit();
 
-  const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=${units}&appid=${WEATHER_API_KEY}`;
+  // const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=${units}&appid=${WEATHER_API_KEY}`;
+
+  // try {
+  //   const wxStream = await fetch(url);
+  //   const wxJson = await wxStream.json();
+  //   return wxJson;
+  // } catch (err) {
+  //   console.log(err);
+  // }
+
+  const urlDataObj = {
+    lat: locationObj.getLat(),
+    lon: locationObj.getLong(),
+    units: locationObj.getUnit(),
+  };
 
   try {
-    const wxStream = await fetch(url);
+    const wxStream = await fetch("./.netlify/functions/get_weather", {
+      method: "POST",
+      body: JSON.stringify(urlDataObj),
+    });
     const wxJson = await wxStream.json();
     return wxJson;
   } catch (err) {
@@ -29,12 +46,27 @@ export const getWeatherFromCoords = async (locationObj) => {
 };
 
 export const getCoordsFromApi = async (entryText, units) => {
-  const regex = /^\d+$/g;
-  const flag = regex.test(entryText) ? "zip?zip" : "direct?q";
-  const url = `http://api.openweathermap.org/geo/1.0/${flag}=${entryText}&appid=${WEATHER_API_KEY}`;
-  const encodedUrl = encodeURI(url);
+  // const regex = /^\d+$/g;
+  // const flag = regex.test(entryText) ? "zip?zip" : "direct?q";
+  // const url = `http://api.openweathermap.org/geo/1.0/${flag}=${entryText}&appid=${WEATHER_API_KEY}`;
+  // const encodedUrl = encodeURI(url);
+  // try {
+  //   const dataStream = await fetch(encodedUrl);
+  //   const jsonData = await dataStream.json();
+  //   return jsonData;
+  // } catch (err) {
+  //   console.log(err.stack);
+  // }
+
+  const urlDataObj = {
+    text: entryText,
+    units: units,
+  };
   try {
-    const dataStream = await fetch(encodedUrl);
+    const dataStream = await fetch("./.netlify/functions/get_coords", {
+      method: "POST",
+      body: JSON.stringify(urlDataObj),
+    });
     const jsonData = await dataStream.json();
     return jsonData;
   } catch (err) {
---
+++ serverless/get_weather.js
@@ -0,0 +1,22 @@
+const fetch = require("node-fetch");
+
+const { WEATHER_API_KEY } = process.env;
+
+exports.handler = async (event, context) => {
+  const params = JSON.parse(event.body);
+  const { lat, lon, units } = params;
+  const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=${units}&appid=${WEATHER_API_KEY}`;
+  try {
+    const wxStream = await fetch(url);
+    const wxJson = await wxStream.json();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(wxJson),
+    };
+  } catch (err) {
+    return {
+      statusCode: 422,
+      body: err.stack,
+    };
+  }
+};
